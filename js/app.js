730e0b181b4beaeccded35783183e49d5074de8d
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-04-12 07:57:11 +0300

    New Sub

--- js/app.js
+++ js/app.js
@@ -1,8 +1,163 @@
 'use strict';
 
-let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','Total'];
+let tableID = document.getElementById('tableID');
+let arrTotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let totals = 0;
 
-function generateNum(min,max) {
+// Generating random numbers 
+function randomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min); }
+  
+
+// Generating the objects by construtors
+
+function Store(location, minCust, maxCust, avgCook) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCook = avgCook;
+  this.custPerHour = [];
+  this.cook = [];
+  this.total = 0;
+}
+
+// first method to randomly calc the customer per hour
+Store.prototype.calccustPerHour = function () {
+  for (let i = 0; i < hours.length - 1; i++) {
+    this.custPerHour.push(randomValue(this.minCust, this.maxCust));
+  }
+};
+
+// second method to calc the cook per hour
+
+Store.prototype.calccookPerhour = function () {
+  for (let i = 0; i < hours.length - 1; i++) {
+    this.cook.push(this.custPerHour[i] * Math.floor(this.avgCook));
+    this.total = this.total + this.cook[i];
+    arrTotal[i] = arrTotal[i] + this.cook[i];
+
+  }
+  this.cook[this.cook.length] = this.total;
+  totals += this.total;
+};
+
+// third method build the body of the table
+
+Store.prototype.createElement = function () {
+  let tableRow = document.createElement('tr');
+  tableID.appendChild(tableRow);
+  let tdEl1 = document.createElement('td');
+  tableRow.appendChild(tdEl1);
+  tdEl1.textContent = this.location;
+  let tdEl;
+  for (let i = 0; i < this.cook.length; i++) {
+    tdEl = document.createElement('td');
+    tableRow.appendChild(tdEl);
+    tdEl.textContent = this.cook[i];
+  }
+};
+
+//  function to create headings
+
+function heading() {
+  let thead = document.createElement('thead');
+  tableID.appendChild(thead);
+  let tableRow = document.createElement('tr');
+  thead.appendChild(tableRow);
+  let tdEl1 = document.createElement('td');
+  tableRow.appendChild(tdEl1);
+  tdEl1.textContent = 'stores';
+  let thEl;
+  for (let i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    tableRow.appendChild(thEl);
+    thEl.textContent = hours[i];
+  }
+}
+
+// functions to create footers
+function footers() {
+  let footer = document.createElement('tfoot');
+  tableID.appendChild(footer);
+  let tableRow = document.createElement('tr');
+  footer.appendChild(tableRow);
+  let tdEl1 = document.createElement('td');
+  tableRow.appendChild(tdEl1);
+  tdEl1.textContent = 'Total';
+  let tdEl;
+  for (let i = 0; i < hours.length-1; i++) {
+    tdEl = document.createElement('td');
+    tableRow.appendChild(tdEl);
+    tdEl.textContent = `${arrTotal[i]}`;
+  }
+  tdEl = document.createElement('td');
+  tableRow.appendChild(tdEl);
+  tdEl.textContent = `${totals}`;
+}
+
+heading();
+
+// Calling to Creating Objects inside the body of tables
+
+let seattle = new Store('Seattle', 23, 65, 6.3);
+seattle.calccustPerHour();
+seattle.calccookPerhour();
+seattle.createElement();
+
+let tokyo = new Store('Tokyo', 3, 24, 1.2);
+tokyo.calccustPerHour();
+tokyo.calccookPerhour();
+tokyo.createElement();
+
+let dubai = new Store('Dubai', 11, 38, 3.7);
+dubai.calccustPerHour();
+dubai.calccookPerhour();
+dubai.createElement();
+
+let paris = new Store('Paris', 20, 38, 2.3);
+paris.calccustPerHour();
+paris.calccookPerhour();
+paris.createElement();
+
+let lima = new Store('Lima', 2, 16, 4.6);
+lima.calccustPerHour();
+lima.calccookPerhour();
+lima.createElement();
+
+footers();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function generateNum(min,max) {
 
   return Math.floor(Math.random()*(max-min+1)+min);
 }
@@ -265,3 +420,4 @@ for (let n= 0; n < lima.numCook.length; n++) {
 listItem = document.createElement('li');
 firstList.appendChild(listItem);
 listItem.textContent = `Total: ${totalCook} cookies`;
+*/
